c302b6f0bf32c48a694432c28aa02bab2a3044f7
tnguyeen <tainguyen.pham.133@gmail.com>
2024-12-19 09:27:10 +0700

    config background tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,7 +58,7 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       backgroundImage: {
-        "main-bg": "url('@/public/bg/bg.png')",
+        "main-bg": "url('/bg/bg.png')",
       },
     },
   },
