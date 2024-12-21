a10a59b86b7d0eba36e51850e33f3764d4cb5ce1
tnguyeen <tainguyen.pham.133@gmail.com>
2024-12-21 13:41:51 +0700

    sửa giao diện và code style

--- src/app/comment/_components/chatbox/Comment.tsx
+++ src/app/comment/_components/chatbox/Comment.tsx
@@ -35,27 +35,27 @@ function Comment({ comment }: { comment: CommentProps }) {
           </div>
         </div>
       </div>
-      {comment.subComment?.map((subComment, index) => (
+      {comment.subComment?.map((sub_comment, index) => (
         <div
           className="flex items-start px-3 gap-3 ml-12"
           key={index}
         >
           <Image
             alt="profile picture"
-            src={subComment.user_profile}
+            src={sub_comment.user_profile}
             className="size-10 rounded-full object-contain "
           />
           <div className="flex flex-col gap-[2px] w-full ">
             <div className="py-1 px-2 rounded-xl bg-slate-100">
               <p className="flex items-center">
                 <span className="text-sm font-bold flex-auto cursor-pointer hover:underline">
-                  {subComment.user_name}
+                  {sub_comment.user_name}
                 </span>
                 <span className="text-xs text-slate-700">
-                  {subComment.comment_time}
+                  {sub_comment.comment_time}
                 </span>
               </p>
-              <span className="text-sm">{subComment.content}</span>
+              <span className="text-sm">{sub_comment.content}</span>
             </div>
             <div className="text-sm flex items-center gap-5">
               <span className="hover:underline cursor-pointer">Trả lời</span>
