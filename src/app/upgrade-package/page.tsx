022e7f664e9c904df1c1e51f48dd414b856c9166
tnguyeen <tainguyen.pham.133@gmail.com>
2024-12-25 10:42:08 +0700

    thêm ui nút đóng modal

--- src/app/upgrade-package/page.tsx
+++ src/app/upgrade-package/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { X } from 'lucide-react'
 import BillInfomation from './_components/BillInfomation'
 import CofirmButtons from './_components/CofirmButtons'
 
@@ -6,6 +7,10 @@ function page() {
   return (
     <section className="size-full center-child p-[17px] ">
       <div className="modal flex flex-col w-full max-w-[760px] rounded-xl border border-slate-200 p-6 gap-4 relative">
+        <X
+          className="absolute right-4 top-4"
+          size={16}
+        />
         <div className="flex flex-col gap-0">
           <span className="text-lg font-semibold leading-relaxed">
             Xác nhận thanh toán
